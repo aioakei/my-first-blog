67c814e98ecfa18b904269c0ad7839389cbeefee
aioakei <aioakei@gmail.com>
2017-08-21 11:13:35 +0200

    fix the setting.py.ALLOWED_HOSTS = [route2sabya.pythonanywhere.com]

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v2rusi*-a%+-+j(23h!no6y7hwo^4g!_ahnirqgf&^&0(vrqts'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [route2sabya.pythonanywhere.com]
 
 
 # Application definition
